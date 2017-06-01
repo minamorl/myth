0a4d0a319f176ab8260fc823985edefdcfb426b7
minamorl <minamorl@users.noreply.github.com>
2017-06-02 01:29:57 +0900

    Better error handling

--- src/vector.rs
+++ src/vector.rs
@@ -14,36 +14,38 @@ impl<T> Vector<T>
             dim: dim,
         }
     }
-    pub fn add(&self, vec: &Self) -> Self {
+    pub fn add(&self, vec: &Self) -> Result<Self, &str> {
         if self.dim != vec.dim {
-            panic!("Vec size is wrong");
+            Err("Vec size is wrong")
+        } else {
+            Ok(Vector::new((0..self.dim).map(|i| self.v[i] + vec.v[i]).collect()))
         }
-        Vector::new((0..self.dim).map(|i| self.v[i] + vec.v[i]).collect())
     }
     pub fn inverse(&self) -> Self {
         let minus: T = -1i32.into();
         self.scalar(minus)
     }
-    pub fn dot(&self, vec: &Self) -> T {
+    pub fn dot(&self, vec: &Self) -> Result<T, &str> {
         if self.dim != vec.dim {
-            panic!("Vec size is wrong");
+            Err("Vec size is wrong")
+        } else {
+            Ok((0..self.dim).map(|i| self.v[i] * vec.v[i]).fold(0.into(), |sum, val| sum + val))
         }
-        (0..self.dim).map(|i| self.v[i] * vec.v[i]).fold(0.into(), |sum, val| sum + val)
     }
-    pub fn cross(&self, vec: &Self) -> Self {
+    pub fn cross(&self, vec: &Self) -> Result<Self, &str> {
         let len1 = self.dim;
         let len2 = vec.dim;
         if len1 != len2 || len1 < 2 || len1 > 3 || len2 < 2 || len2 > 3 {
-            panic!("Vec size should be 2 or 3");
+            return Err("Vec size should be 2 or 3");
         }
         if len1 == 2 {
-            Self::new(vec![self.v[0] * vec.v[1] - self.v[1] * vec.v[0]]) 
+            Ok(Self::new(vec![self.v[0] * vec.v[1] - self.v[1] * vec.v[0]])) 
         } else {
-            Self::new(vec![
+            Ok(Self::new(vec![
                 self.v[1] * vec.v[2] - self.v[2] * vec.v[1],
                 self.v[2] * vec.v[0] - self.v[0] * vec.v[2],   
                 self.v[0] * vec.v[1] - self.v[1] * vec.v[0],
-            ])
+            ]))
         }
     }
     pub fn scalar<U>(&self, n: U) -> Self where U: num::Signed + Into<T> + Copy + Clone { 
@@ -61,7 +63,7 @@ mod tests {
     fn test_vector_add() {
         let v1 = Vector::new(vec![1, 2, 3]);
         let v2 = Vector::new(vec![1, 2, 3]);
-        let v3 = v1.add(&v2);
+        let v3 = v1.add(&v2).unwrap();
         assert_eq!(v3.v, vec![2, 4, 6]);
     }
 
@@ -69,7 +71,7 @@ mod tests {
     fn test_vector_dot() {
         let v1 = Vector::new(vec![1, 2, 3]);
         let v2 = Vector::new(vec![1, 2, 3]);
-        let r = v1.dot(&v2);
+        let r = v1.dot(&v2).unwrap();
         assert_eq!(r, 14);
     }
 
@@ -84,7 +86,7 @@ mod tests {
     fn test_vector_cross() {
         let v1 = Vector::new(vec![1, 2, 3]);
         let v2 = Vector::new(vec![1, 2, 3]);
-        let r = v1.cross(&v2);
+        let r = v1.cross(&v2).unwrap();
         assert_eq!(r.v, vec![2 * 3 - 3 * 2, 3 * 1 - 1 * 3, 1 * 2 - 2 * 1]);
     }
 
