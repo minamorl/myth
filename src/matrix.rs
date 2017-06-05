f1e5b02be6536878af73b3d3eb71b4ec60cb5b7f
minamorl <minamorl@users.noreply.github.com>
2017-06-05 20:40:57 +0900

    Add Matrix.pivot(), decompose() and zeroes()

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,7 @@
 extern crate num;
 use std::iter::{Sum};
 use std::ops::Mul;
+use std::cmp::{Ord};
 
 #[derive(Debug)]
 pub struct Matrix<T> {
@@ -9,13 +10,18 @@ pub struct Matrix<T> {
 }
 
 impl<T> Matrix<T> 
-    where T: num::Signed + Clone + Copy + Sum + Mul, i32: Into<T> {
+    where T: num::Signed + Clone + Copy + Sum + Mul + PartialOrd, i32: Into<T> {
     pub fn new(v: Vec<Vec<T>>) -> Self {
         let size = (v.len(), v[0].len());
         Self {
             v: v,
             size: size,
         }
+    }
+    pub fn zeroes(n: usize, m: usize) -> Self {
+        Self::new(
+            vec![vec![0.into(); m]; n]
+        )
     }
     pub fn diag(v: Vec<T>) -> Self {
         Self::new(
@@ -71,6 +77,35 @@ impl<T> Matrix<T>
                 l.iter().map(|x| (*x) * n.into()).collect()
             }).collect()
         )
+    }
+    pub fn pivot(&self) -> Self {
+        let mut pivot = Self::diag(vec![1.into(); self.size.0]);
+
+        for j in 0..self.size.0 {
+            for i in j..self.size.0 {
+                if self.v[i][j].abs() > self.v[j][j].abs() {
+                    pivot.v.swap(i, j);
+                }
+           }
+        }
+        pivot
+
+    }
+    /// Provides a result from LU decomposition
+    pub fn decompose(&self) -> (Self, Self){
+        let mut L = Matrix::diag(vec![1.into(); self.size.0]);
+        let mut U = Matrix::zeroes(self.size.0, self.size.0);
+        let P = self.pivot();
+        let PA = P.mul(&self).unwrap();
+        for j in 0..self.v.len() {
+            for i in 0..j + 1 {
+                U.v[i][j] = PA.v[i][j] - (0..i).map(|k| U.v[k][j] * L.v[i][k]).sum()
+            }
+            for i in j..self.size.0 {
+                L.v[i][j] = (PA.v[i][j] - (0..j).map(|k| L.v[i][k] * U.v[k][j]).sum()) / U.v[j][j]
+            }
+        }
+        (P, L, U)
     }
 }
 
@@ -144,5 +179,30 @@ mod tests {
     #[test]
     fn test_matrix_scalar() {
         assert_eq!(Matrix::diag(vec![1; 3]).scalar(2).v, Matrix::diag(vec![2; 3]).v);
+    }
+    #[test]
+    fn test_matrix_decompose() {
+        let (P, L, U) = Matrix::new(
+            vec![
+                vec![7.0, 3.0, -1.0, 2.0],
+                vec![3.0, 8.0, 1.0, -4.0],
+                vec![-1.0, 1.0, 4.0, -1.0],
+                vec![2.0, -4.0, -1.0, 6.0],
+            ]
+        ).decompose();
+
+        assert_eq!(L.v, vec![
+            vec![1.0, 0.0, 0.0, 0.0],
+            vec![0.42857142857142855, 1.0, 0.0, 0.0],
+            vec![-0.14285714285714285, 0.2127659574468085, 1.0, 0.0],
+            vec![0.2857142857142857, -0.7234042553191489, 0.0898203592814371, 1.0]
+        ]);
+
+        assert_eq!(U.v, vec![
+            vec![7.0, 3.0, -1.0, 2.0], 
+            vec![0.0, 6.714285714285714, 1.4285714285714286, -4.857142857142857],
+            vec![0.0, 0.0, 3.5531914893617023, 0.31914893617021267],
+            vec![0.0, 0.0, 0.0, 1.88622754491018],
+        ]);
     }
 }
